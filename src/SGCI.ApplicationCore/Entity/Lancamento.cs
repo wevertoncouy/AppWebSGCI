1fe2073d075f4456f5d3939c63d705184966611b
Graça <Graa@GRAA-PC>
2018-04-13 17:56:50 -0300

    Update

---
+++ src/SGCI.ApplicationCore/Entity/Lancamento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGCI.Infrastructure.Data
+{
+  public  class Lancamento
+    {
+        public Lancamento()
+        {
+
+        }
+
+        public int LancamentoId { get; set; }
+        public DateTime Data { get; set; }
+        public int CategoriaId { get; set; }//fk
+        public int ContaId { get; set; }
+
+        public decimal Valor { get; set; }
+        public Decimal Resultado { get; set; }
+        public string Descricao { get; set; }
+
+        // public Categoria Categoria { get; set; }
+
+       // public Conta Conta { get; set; }
+    }
+}
---
+++ src/SGCI.Infrastructure/Data/GestaoCarteiraContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGCI.ApplicationCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGCI.Infrastructure.Data
+{
+    public class GestaoCarteiraContext : DbContext
+    {
+        public GestaoCarteiraContext(DbContextOptions<GestaoCarteiraContext> options) : base(options)
+        {
+
+        }
+        // mapeamento
+        public DbSet<Carteira> Carteiras { get; set; }
+        public DbSet<Conta> Contas { get; set; }
+
+        ///Fluent Api
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Carteira>().ToTable("Carteira");
+            modelBuilder.Entity<Conta>().ToTable("Conta");
+
+            #region Configurations de Carteira
+
+            modelBuilder.Entity<Carteira>().Property(e => e.Descricao)
+               .HasColumnType("varchar(230)")
+                .IsRequired();
+
+
+
+            #endregion
+            
+            #region Configurations de Conta
+
+            modelBuilder.Entity<Conta>().Property(e => e.Nome)
+               .HasColumnType("varchar(150)")
+                .IsRequired();
+            #endregion
+
+
+            #region Configurations Menu
+            //modelBuilder.Entity<Menu>()
+            // .HasMany(x => x.SubMenu)
+            // .WithOne()
+            // .HasForeignKey(x => x.MenuId);
+            #endregion
+        }
+
+
+
+
+    }
+}
